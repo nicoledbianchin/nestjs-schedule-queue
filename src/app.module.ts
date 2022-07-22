a232b26f94da8b87e9bf44a408a8d61c64baad65
nicoledbianchin <nicoledbianchin@gmail.com>
2022-07-22 17:20:17 -0300

    alteracoes para testes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseService } from './database/database.service';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
+  imports: [TweetsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, DatabaseService],
 })
 export class AppModule {}
