e8ee07d4a596496c7eff766123f8d6d7aabb97af
nicoledbianchin <nicoledbianchin@gmail.com>
2022-07-26 14:04:48 -0300

    implementa tweet service para criar e buscar tweets

--- src/tweets/entities/tweet.entity.ts
+++ src/tweets/entities/tweet.entity.ts
@@ -2,5 +2,5 @@ import { Column, Model, Table } from 'sequelize-typescript'
 @Table({ tableName: 'tweets' })
 export class Tweet extends Model {
     @Column
-    name: string;
+    text: string;
 }
--- src/tweets/tweets.service.ts
+++ src/tweets/tweets.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 import { CreateTweetDto } from './dto/create-tweet.dto';
 import { UpdateTweetDto } from './dto/update-tweet.dto';
+import { Tweet } from './entities/tweet.entity';
 
 @Injectable()
 export class TweetsService {
+
+  constructor(@InjectModel(Tweet) private tweetModel: typeof Tweet) {
+  }
   create(createTweetDto: CreateTweetDto) {
-    return 'This action adds a new tweet';
+    return this.tweetModel.create(createTweetDto as any);
   }
 
   findAll() {
-    return `This action returns all tweets`;
+    return this.tweetModel.findAll();
   }
 
   findOne(id: number) {
