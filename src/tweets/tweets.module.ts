caa6b6afbb121be72fd1fe7e0cd0ab8b584910af
nicoledbianchin <nicoledbianchin@gmail.com>
2022-07-26 15:07:57 -0300

    gera service para contar tweets

--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -3,12 +3,13 @@ import { TweetsService } from './tweets.service';
 import { TweetsController } from './tweets.controller';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Tweet } from './entities/tweet.entity';
+import { TweetsCountService } from './tweets-count/tweets-count.service';
 
 @Module({
   imports: [
     SequelizeModule.forFeature([Tweet])
   ],
   controllers: [TweetsController],
-  providers: [TweetsService]
+  providers: [TweetsService, TweetsCountService]
 })
 export class TweetsModule { }
