0478a1e24d9ba6e2ceced2ec8c1db0a8c6a8a3a2
nicoledbianchin <nicoledbianchin@gmail.com>
2022-07-26 13:48:30 -0300

    termina de construir módulo e entidades

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseService } from './database/database.service';
 import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [TweetsModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true
+    }),
+    TweetsModule],
   controllers: [AppController],
   providers: [AppService, DatabaseService],
 })
-export class AppModule {}
+export class AppModule { } 
